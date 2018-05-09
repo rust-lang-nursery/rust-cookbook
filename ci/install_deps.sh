d08ee136f5619257bde493d2d6ad3e54739f2c3a
Michal Budzynski <budziq@gmail.com>
2018-05-10 01:04:46 +0200

    Added support for additional linters (html-proofer link checker)
    - added additional test matrix item just for linters
    - install and build mdbook only when needed to speedup CI
    - reorganized the travis scripts

---
+++ ci/install_deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+echo "Running $0"
+
+if [ -z "${TRAVIS_BRANCH:-}" ]; then
+    echo "This script may only be run from Travis!"
+    exit 1
+fi
+
+if [[ "${CONTENT_TESTS:-}" == 1 ]]; then
+    echo "Installing additional dependencies"
+    gem install html-proofer
+    cargo install mdbook --vers '0.1.7' --debug
+fi
+
+exit 0
+
---
+++ ci/test_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+echo "Running $0"
+
+if [ -z "${TRAVIS_BRANCH:-}" ]; then
+    echo "This script may only be run from Travis!"
+    exit 1
+fi
+
+# Returns 1 if program is installed and 0 otherwise
+program_installed() {
+    local return_=1
+
+    type $1 >/dev/null 2>&1 || { local return_=0; }
+
+    echo "$return_"
+}
+
+if [[ "${CONTENT_TESTS:-}" == 1 ]]; then
+    # Ensure required programs are installed
+    if [ $(program_installed htmlproofer) == 0 ]; then
+        echo "Please install htmlproofer: gem install html-proofer"
+        exit 1
+    elif [ $(program_installed mdbook) == 0 ]; then
+        echo "Please install mdbook: cargo install mdbook."
+        exit 1
+    fi
+
+    echo "Testing markup and descriptions"
+    echo "Building site to book/"
+    mdbook build
+    htmlproofer --empty-alt-ignore ./book/
+else
+    echo "Testing code snippets"
+    cargo build --verbose
+    cargo test --verbose
+fi
