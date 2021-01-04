46fdc71d6782494fbd558d80d952bf0f41162fae
Andrew Gauger <andygauge@gmail.com>
2021-01-04 19:02:01 +0000

    update mdbook

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -18,7 +18,7 @@ if [[ "${CONTENT_TESTS:-}" == 1 ]]; then
         pyenv local 3.6.0
         pip3 install --user link-checker==0.1.0
     fi
-    cargo install mdbook --vers '0.3.5' --debug
+    cargo install mdbook --vers '0.4.5' --debug
 fi
 
 exit 0
