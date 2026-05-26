d9a1731c54cb91dc39477c2722adcd5d7de4c146
AndyGauge <AndyGauge@users.noreply.github.com>
2026-05-26 23:22:00 +0000

    deploy: 1461623be2b22fdc41cb71b7c68a5f0e18d2049b

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a1094d1f.js',
+                path_to_root + 'searchindex-598a8977.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
